aebc1f8a6f23fd4bd9a931b901eaf86772edeff0
mac_a2141 <victor@piousbox.com>
2023-03-04 16:31:30 -0500

    little

--- config/environments/development/config.js
+++ config/environments/development/config.js
@@ -3,7 +3,9 @@ module.exports = {
   apiOrigin: 'http://localhost:3001',
 
   router: {
+    leadsetsPath: '/api/leadsets',
     loginPath: '/api/users/login',
+
     myProfilePath: '/api/users/profile/update',
   },
 
